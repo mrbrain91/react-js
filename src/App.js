11d01b031b6b976af3cce24fb495437c71663685
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-06-25 19:17:14 +0500

    done 123-ELementi i komponenti

--- src/App.js
+++ src/App.js
@@ -1,20 +1,56 @@
-import logo from './logo.svg';
+import {Component} from 'react';
 import './App.css';
+
+const Header = () => {
+  return <h2>Hello world!</h2>
+}
+
+const Field = () => {
+  const holder = 'Enter here...';
+  const styledField = {
+    width: '300px'
+  };
+  return <input 
+          placeholder={holder} 
+          type="text" 
+          style={styledField} />
+}
+
+// class Field extends Component {
+//         render() {
+//           const holder = 'Enter here...';
+//           const styledField = {
+//             width: '300px'
+//       };
+
+//       return <input 
+//       placeholder={holder} 
+//       type="text" 
+//       style={styledField} />
+//   }
+// }
+
+function Btn() {
+
+  const text = 'Log in';
+  const logged = true;
+
+  // const res = () => {
+  //   return 'Log in';
+  // }
+  // const p = <p>Log in</p>
+  return <button>{logged ? 'Enter' : text}</button>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Editt <code>src/App.js</code> and save to reload.
-          
-        </p>
-       
-
-      </header>
+        <Header/>
+        <Field/>
+        <Btn/>
     </div>
   );
 }
 
+export {Header};
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,23 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { Header } from './App';
 
-const text = 'Hello world';
-
-// const elem = <h2>hello world!</h2>;
-const elem = (
-  <div>
-    <h2 className='asas'>Text: {text}</h2>
-    <input type="text" />
-    <label htmlFor='asd'>asdasdas</label>
-    <button>Click</button>
-  </div>
-)
-
-// const elem = React.createElement('h2', {className: 'greetings'}, 'Hello world!'); eski usul
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  elem
+  <App/>
 );
 
