ac661f958bdcb3396b3576bbf045dec423a7e465
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-06-26 13:55:56 +0500

    done 124-125 - Strogiy rejim Strict mode

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, {StrictMode} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import { Header } from './App';
+// import { Header } from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App/>
+  <StrictMode>
+     <App/>
+  </StrictMode>
 );
 
