d364be5107d6c780f8864eb0bb79ac6e9f4e30b6
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-06-29 21:02:30 +0500

    done 133 - Praktika sostoyaniy na praktike

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,14 @@
+import './app-info.css';
+
+const AppInfo = () => {
+    return (
+        <div className="app-info">
+            <h1>Учет сотрудников в компании N</h1>
+            <h2>Obshee chislo sotrudnikov: </h2>
+            <h2>Premiyu poluchat: </h2>
+        </div>
+    )
+}
+
+
+export default AppInfo;
