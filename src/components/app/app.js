8563b281cff0194bb7aa832ddacca6476bad2e08
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-07-02 20:05:55 +0500

    done 135-immutabelnost sostoyanie i sobstvennoe sobitiya with home work

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,3 +1,5 @@
+import { Component } from 'react';
+
 import AppInfo from '../app-info/app-info';
 import SearchPanel from '../search-panel/search-panel';
 import AppFilter from '../app-filter/app-filter';
@@ -6,27 +8,73 @@ import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 import './app.css'
 
-function App() {
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data : [
+                {name: 'John C.', salary: 800, increase: false, id: 1},
+                {name: 'Alex M.', salary: 3000, increase: true, id: 2},
+                {name: 'Carl W.', salary: 5000, increase: false, id: 3}
+            ]
+        }
+        this.maxId = 4;
+    }
+// delete user
+    deleteItem = (id) => {
+        this.setState(({data}) => {
+            // const index = data.findIndex(elem => elem.id === id);
 
-    const data = [
-        {name: 'John C.', salary: 800, increase: false, id: 1},
-        {name: 'Alex M.', salary: 3000, increase: true, id: 2},
-        {name: 'Carl W.', salary: 5000, increase: false, id: 3}
-    ];
+            //wrong way
+            // data.splice(index, 1);
 
-    return (
-        <div className='app'> 
-            <AppInfo/>
 
-            <div className="search-panel">
-                <SearchPanel/>
-                <AppFilter/>
+            // right way 1
+            // const before = data.slice(0, index);
+            // const after = data.slice(index + 1)
+
+            // const newArr = [...before, ...after];
+
+            //right way 2
+          
+            return {
+                data: data.filter(item => item.id !== id)
+            }
+        })
+    }
+// add new user
+    addItem = (name, salary) => {
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            id: this.maxId++
+        }
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data : newArr
+            }
+        })
+    }
+
+    render() {
+        return (
+            <div className='app'> 
+                <AppInfo/>
+    
+                <div className="search-panel">
+                    <SearchPanel/>
+                    <AppFilter/>
+                </div>
+    
+                <EmployeesList 
+                    data={this.state.data}
+                    onDelete={this.deleteItem}/>
+                <EmployeesAddForm onAdd={this.addItem}/>
             </div>
-
-            <EmployeesList data={data}/>
-            <EmployeesAddForm/>
-        </div>
-    )
+        )
+    }
 }
 
 export default App;
