c2dc43440613415388c5d73f9a2e4015ee6e5fd1
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-07-10 12:17:05 +0500

    done 139-140 semantika + Stili v React. Inline styles

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -20,7 +20,10 @@ const EmployeesListItem = (props) => {
 
         return (
             <li className={classNames}>
-                <span className="list-group-item-label"  onClick={onToggleProp} data-toggle="rise">{name}</span>
+                <span className="list-group-item-label"  
+                      onClick={onToggleProp} 
+                      data-toggle="rise"
+                      style={{fontSize: 20, transition: 'all'}}>{name}</span>
                 <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
                 <div className='d-flex justify-content-center align-items-center'>
                     <button type="button"
