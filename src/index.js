eb72740b9b697b1133d7ca6991b1440c5af14a8f
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-07-11 07:15:17 +0500

    done 144-Stili v react. Gotovie biblioteki componentov so stilami

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import {Button} from './App';
+import BootstrapTest from './BootstrapTest';
 import styled from 'styled-components' ;
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 const BigButton = styled(Button)`
@@ -17,6 +20,7 @@ root.render(
   <React.StrictMode>
     <App />
     <BigButton as='a'>Otpravit otchet</BigButton>
+    <BootstrapTest/>
   </React.StrictMode>
 );
 
