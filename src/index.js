f77af29c139d888b9935392687fc2be2296c56c5
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-07-11 08:09:40 +0500

    done 145-Polya klassov i static

--- src/index.js
+++ src/index.js
@@ -1,26 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import {Button} from './App';
-import BootstrapTest from './BootstrapTest';
-import styled from 'styled-components' ;
-
-import 'bootstrap/dist/css/bootstrap.min.css';
-
-
-const BigButton = styled(Button)`
-  margin: 0 auto;
-  width: 245px;
-  text-align: center;
-`;
+import './index.scss';
+import App from './components/app/app';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <BigButton as='a'>Otpravit otchet</BigButton>
-    <BootstrapTest/>
   </React.StrictMode>
 );
 
