68c92a63bc64e2adc44687d9dc16776d92a7fff6
Fazliddin <f.yuldashev.tuit@gmail.com>
2022-07-12 23:59:29 +0500

    done 149 - Transformatsiya dannx i komponent so sluchaynm personajom

--- src/index.js
+++ src/index.js
@@ -4,13 +4,6 @@ import App from './components/app/App';
 import MarvelService from './services/MarvelService';
 import './style/style.scss';
 
-const marvelService = new MarvelService();
-
-marvelService.getAllCharacters().then(res => res.data.results.forEach(item => console.log(item.name)));
-
-// marvelService.getCharacter(1011053).then(res => console.log(res));
-
-
 ReactDOM.render(
   <React.StrictMode>
     <App />
